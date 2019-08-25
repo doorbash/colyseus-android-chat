3259a7f47cb1f01efbb3138cbb0edf521c404b75
Milad Doorbash <milad.doorbash@gmail.com>
2019-08-25 14:37:32 +0430

    update code to colyseus-java:2.3.1

--- client/app/src/main/java/ir/doorbash/colyseus_chat/MainActivity.java
+++ client/app/src/main/java/ir/doorbash/colyseus_chat/MainActivity.java
@@ -1,6 +1,5 @@
 package ir.doorbash.colyseus_chat;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.SparseArray;
@@ -17,8 +16,7 @@ import java.util.List;
 
 import io.colyseus.Client;
 import io.colyseus.Room;
-import io.colyseus.serializer.schema.DataChange;
-import io.colyseus.serializer.schema.Schema;
+import io.colyseus.serializer.schema.Change;
 import ir.doorbash.colyseus_chat.classes.Message;
 import ir.doorbash.colyseus_chat.classes.MyState;
 import ir.doorbash.colyseus_chat.classes.User;
@@ -34,8 +32,6 @@ public class MainActivity extends AppCompatActivity {
     TextView typing;
 
     // Variables
-    String clientId;
-    Client client;
     Room<MyState> room;
     MessagesListAdapter<Message> adapter;
     final HashMap<String, User> users = new HashMap<>();
@@ -50,17 +46,14 @@ public class MainActivity extends AppCompatActivity {
         inputView = findViewById(R.id.input);
         typing = findViewById(R.id.typing);
 
-        inputView.setInputListener(new MessageInput.InputListener() {
-            @Override
-            public boolean onSubmit(CharSequence input) {
-                if (room != null) {
-                    LinkedHashMap<String, Object> data = new LinkedHashMap<>();
-                    data.put("op", "message");
-                    data.put("message", input.toString());
-                    room.send(data);
-                }
-                return true;
+        inputView.setInputListener(input -> {
+            if (room != null) {
+                LinkedHashMap<String, Object> data = new LinkedHashMap<>();
+                data.put("op", "message");
+                data.put("message", input.toString());
+                room.send(data);
             }
+            return true;
         });
 
         inputView.setTypingListener(new MessageInput.TypingListener() {
@@ -85,118 +78,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        SharedPreferences prefs = getSharedPreferences("shared_prefs", MODE_PRIVATE);
-        clientId = prefs.getString("colyseus-id", null);
-
         connectToServer();
     }
 
     private void connectToServer() {
-        client = new Client(ENDPOINT, clientId, new Client.Listener() {
-            @Override
-            public void onOpen(final String id) {
-                if (clientId == null) {
-                    clientId = id;
-                    SharedPreferences.Editor editor = getSharedPreferences("shared_prefs", MODE_PRIVATE).edit();
-                    editor.putString("colyseus-id", clientId);
-                    editor.apply();
+        Client client = new Client(ENDPOINT);
+        client.joinOrCreate("chat", MyState.class, room -> {
+            System.out.println("joined chat");
+            this.room = room;
+            runOnUiThread(() -> {
+                adapter = new MessagesListAdapter<>(room.getSessionId(), null);
+                messagesList.setAdapter(adapter);
+            });
+            room.state.users.onAdd = (user, key) -> {
+                synchronized (users) {
+                    users.put(key, user);
                 }
-
-                if (adapter == null) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            adapter = new MessagesListAdapter<>(id, null);
-                            messagesList.setAdapter(adapter);
+                user.onChange = changes -> {
+                    for (Change change : changes) {
+                        if (change.field.equals("is_typing")) {
+                            System.out.println(room.getSessionId() + " : " + change);
+                            runOnUiThread(this::updateTypingUI);
                         }
-                    });
+                    }
+                };
+            };
+            room.state.users.onRemove = (value, key) -> {
+                synchronized (users) {
+                    users.remove(key);
                 }
-
-                room = client.join("chat", MyState.class);
-                room.addListener(new Room.Listener() {
-                    @Override
-                    protected void onLeave() {
-                        System.out.println("left chat");
-                    }
-
-                    @Override
-                    protected void onError(Exception e) {
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    protected void onMessage(Object message) {
-                        System.out.println(message);
-                    }
-
-                    @Override
-                    protected void onJoin() {
-                        System.out.println("joined chat");
-                        room.state.users.onAdd = new Schema.MapSchema.onAddListener<User>() {
-                            @Override
-                            public void onAdd(User user, String key) {
-                                synchronized (users) {
-                                    users.put(key, user);
-                                }
-                                user.onChange = new Schema.onChange() {
-                                    @Override
-                                    public void onChange(List<DataChange> changes) {
-                                        for (DataChange change : changes) {
-                                            if (change.field.equals("is_typing")) updateTypingUI();
-                                        }
-                                    }
-                                };
-                            }
-                        };
-                        room.state.users.onRemove = new Schema.MapSchema.onRemoveListener<User>() {
-                            @Override
-                            public void onRemove(User value, String key) {
-                                synchronized (users) {
-                                    users.remove(key);
-                                }
-                            }
-                        };
-                        room.state.messages.onAdd = new Schema.ArraySchema.onAddListener<Message>() {
-                            @Override
-                            public void onAdd(final Message message, int key) {
-                                synchronized (messages) {
-                                    messages.put(key, message);
-                                }
-                                message.senderUser = room.state.users.get(message.sender);
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        adapter.addToStart(message, true);
-                                    }
-                                });
-                            }
-                        };
-                        room.state.messages.onRemove = new Schema.ArraySchema.onRemoveListener<Message>() {
-                            @Override
-                            public void onRemove(Message value, int key) {
-                                synchronized (messages) {
-                                    messages.remove(key);
-                                }
-                            }
-                        };
-                    }
-                });
-            }
-
-            @Override
-            public void onMessage(Object message) {
-            }
-
-            @Override
-            public void onClose(int code, String reason, boolean remote) {
-                System.out.println("client closed");
-            }
-
-            @Override
-            public void onError(Exception e) {
-                e.printStackTrace();
-            }
-        });
+            };
+            room.state.messages.onAdd = (message, key) -> {
+                synchronized (messages) {
+                    messages.put(key, message);
+                }
+                message.senderUser = room.state.users.get(message.sender);
+                runOnUiThread(() -> adapter.addToStart(message, true));
+            };
+            room.state.messages.onRemove = (value, key) -> {
+                synchronized (messages) {
+                    messages.remove(key);
+                }
+            };
+            room.state.users.triggerAll();
+            room.state.messages.triggerAll();
+        }, Throwable::printStackTrace);
     }
 
     private void updateTypingUI() {
@@ -204,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
             List<String> typingUsers = new ArrayList<>();
             for (User user : users.values()) {
                 if (user == null) continue;
-                if (!user.id.equals(clientId) && user.is_typing) {
+                if (!user.id.equals(room.getSessionId()) && user.is_typing) {
                     typingUsers.add(user.name);
                 }
             }
@@ -230,7 +156,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         if (room != null) room.leave();
-        if (client != null) client.close();
         super.onDestroy();
     }
 }
